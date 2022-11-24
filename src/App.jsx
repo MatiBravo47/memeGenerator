e7b659a096313c14878e069697b71bc6617f3ef5
MatiBravo47 <matibravoneron@gmail.com>
2022-11-24 14:33:42 -0300

    delete useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
-import ElegirImg from './components/Imgmeme'
 import Imgmeme from './components/Imgmeme'
-import Textomeme from './components/Textomeme'
-import Exportacion from './components/Exportacion'
 
 function App() {
   return (
--- src/components/Imgmeme.jsx
+++ src/components/Imgmeme.jsx
@@ -35,25 +35,24 @@ const Imgmeme = ()=>{
     }
 
     return(
-        <div className='text-center container'>
+      <div className='text-center container'>
         <div className='row d-flex justify-content-center'>
-        <div className='col-8 col-sm-18 col-md-6'>
+          <div className='col-8 col-sm-18 col-md-6'>
             <h1 className='mt-3 mb-3 text-center'>Editá tu propio meme</h1>
-
             <h3 className='mt-3 mb-3 text-center'>Elegí tu meme</h3>
             <select onChange={seleccionarImg} className='form-select form-select-lg mb-3 w-50 m-auto' arial-label=".form-select-lg example" >
-                <option value={1}>Coreanita</option>
-                <option value={2}>Bob esponja</option>
-                <option value={3}>Futurama</option>
-                <option value={4}>Pikachu</option>
-                <option value={5}>Sainz</option>
+              <option value={1}>Coreanita</option>
+              <option value={2}>Bob esponja</option>
+              <option value={3}>Futurama</option>
+              <option value={4}>Pikachu</option>
+              <option value={5}>Sainz</option>
             </select>
             <div className='d-flex justify-content-center'>
-            <figure className="position-relative bg-dark" id="exportar">
+              <figure className="position-relative bg-dark" id="exportar">
                 <p className="w-100 px-40 position-absolute top-0 start-25 h2 text-white">{toptextmeme} </p>
                 <p className="w-100 px-40 position-absolute bottom-0 start-25 h2 text-white">{bottomtextmeme} </p> 
                 <img src={`./memes/${imgmeme}.jpg`} className="figure-img w-100 h-80 mt-5 mb-5 d-block m-auto" alt="meme" />
-            </figure>
+              </figure>
             </div>
             <h3 className='mt-3 mb-3 text-center'>Ingrese el texto del meme</h3>
             <input onChange={topText} className='form-control w-50 m-50 m-auto d-block' type="text" placeholder="Texto superior" name="meme" arial-label="default input example" ></input>
@@ -61,9 +60,9 @@ const Imgmeme = ()=>{
 
             <button onClick={Descargar} type="button" className='btn btn-primary mt-3 mb-4'>Descargar meme</button>
 
+          </div>
         </div>
-        </div>
-        </div>
+      </div>
     );
 
 }
