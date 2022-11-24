ab403f5c1ce51bc5fe7a9d0827ddedb3daddbce0
MatiBravo47 <matibravoneron@gmail.com>
2022-11-24 14:17:46 -0300

    add template, change title

--- src/components/Imgmeme.jsx
+++ src/components/Imgmeme.jsx
@@ -13,11 +13,11 @@ const Imgmeme = ()=>{
         console.log(e.target.value);
     }
 
-    const textomeme = (e) => {
+    const topText = (e) => {
         setTextmeme(e.target.value);
         console.log(e.target.value);
     }
-    const textomeme2 = (e) => {
+    const bottomText = (e) => {
         setTextmeme2(e.target.value);
         console.log(e.target.value);
     }
@@ -37,7 +37,7 @@ const Imgmeme = ()=>{
     return(
         <div className='text-center container'>
         <div className='row d-flex justify-content-center'>
-        <div className='col-12 col-sm-12 col-md-6'>
+        <div className='col-8 col-sm-18 col-md-6'>
             <h1 className='mt-3 mb-3 text-center'>Editá tu propio meme</h1>
 
             <h3 className='mt-3 mb-3 text-center'>Elegí tu meme</h3>
@@ -46,6 +46,7 @@ const Imgmeme = ()=>{
                 <option value={2}>Bob esponja</option>
                 <option value={3}>Futurama</option>
                 <option value={4}>Pikachu</option>
+                <option value={5}>Sainz</option>
             </select>
             <div className='d-flex justify-content-center'>
             <figure className="position-relative bg-dark" id="exportar">
@@ -55,10 +56,10 @@ const Imgmeme = ()=>{
             </figure>
             </div>
             <h3 className='mt-3 mb-3 text-center'>Ingrese el texto del meme</h3>
-            <input onChange={textomeme} className='form-control w-50 m-50 m-auto d-block' type="text" placeholder="Texto superior" name="meme" arial-label="default input example" ></input>
-            <input onChange={textomeme2} className='form-control w-50 m-50 m-auto d-block mt-3' type="text" placeholder="Texto inferior" name="meme" arial-label="default input example" ></input>
+            <input onChange={topText} className='form-control w-50 m-50 m-auto d-block' type="text" placeholder="Texto superior" name="meme" arial-label="default input example" ></input>
+            <input onChange={bottomText} className='form-control w-50 m-50 m-auto d-block mt-3' type="text" placeholder="Texto inferior" name="meme" arial-label="default input example" ></input>
 
-            <button onClick={Descargar} type="button" className='btn btn-primary mt-4 mb-4'>Descargar meme</button>
+            <button onClick={Descargar} type="button" className='btn btn-primary mt-3 mb-4'>Descargar meme</button>
 
         </div>
         </div>
