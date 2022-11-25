1f7ad8f3c2ce07d5ca5f65aece56f91d1e4b11fb
MatiBravo47 <matibravoneron@gmail.com>
2022-11-25 14:40:20 -0300

    fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
       <nav class="navbar bg-dark">
         <div class="container-fluid d-flex align-items-center">
           <a class="navbar-brand text-white" href="#">
-          <img src="./memes/gato.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-top"/>
+          <img src="./memes/gato.png" alt="Logo" width="50" height="50" class="d-inline-block align-text-center"/>
           MemeEditor
           </a>
         </div>
