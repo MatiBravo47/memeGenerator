63271c67a823a3c5d6acfb64d015db71891ac7b8
MatiBravo47 <matibravoneron@gmail.com>
2022-11-25 10:54:33 -0300

    change background-color

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Imgmeme from './components/Imgmeme';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-dark">
       <Imgmeme/>
     </div>
   )
--- src/components/Imgmeme.jsx
+++ src/components/Imgmeme.jsx
@@ -59,8 +59,8 @@ const Imgmeme = ()=>{
       <div className='text-center container'>
         <div className='row d-flex justify-content-center'>
           <div className='col-8 col-sm-18 col-md-6'>
-            <h1 className='mt-3 mb-3 text-center'>Editá tu propio meme</h1>
-            <h3 className='mt-3 mb-3 text-center'>Elegí tu meme</h3>
+            <h1 className='mt-3 mb-3 text-center text-white'>Editá tu propio meme</h1>
+            <h3 className='mt-3 mb-3 text-center text-white'>Elegí tu meme</h3>
             <select onChange={seleccionarImg} className='form-select form-select-lg mb-3 w-50 m-auto' arial-label=".form-select-lg example" >
               <option value={1}>Coreanita</option>
               <option value={2}>Bob esponja</option>
@@ -68,20 +68,20 @@ const Imgmeme = ()=>{
               <option value={4}>Pikachu</option>
               <option value={5}>Sainz</option>
             </select>
-            <div className='d-flex justify-content-center'>
+            <div className='d-flex justify-content-center border border-light'>
               <figure className="position-relative bg-dark" id="exportar">
                 <p className={ `${color} w-100 px-40 position-absolute top-0 start-25 h2`}>{toptextmeme} </p>
                 <p className={`${color} w-100 px-40 position-absolute bottom-0 start-25 h2`}>{bottomtextmeme} </p> 
                 <img src={`./memes/${imgmeme}.jpg`}  className={`figure-img w-100 h-80 ${margen} d-block m-auto`} alt="meme" />
               </figure>
             </div>
-            <h3 className='mt-3 mb-3 text-center'>Ingrese el texto del meme</h3>
+            <h3 className='mt-3 mb-3 text-center text-white'>Ingrese el texto del meme</h3>
             <input onChange={topText} className='form-control w-50 m-50 m-auto d-block' type="text" placeholder="Texto superior" name="meme" arial-label="default input example" ></input>
             <input onChange={bottomText} className='form-control w-50 m-50 m-auto d-block mt-3' type="text" placeholder="Texto inferior" name="meme" arial-label="default input example" ></input>
 
             <button onClick={Descargar} type="button" className='btn btn-primary mt-3 mb-4'>Descargar meme</button>
             <div>
-            <h3 className='mt-3 mb-3 text-center'>Elegí color de texto</h3>
+            <h3 className='mt-3 mb-3 text-center text-white'>Elegí color de texto</h3>
             <div>
               { renderButtons(colors) }
             </div>
